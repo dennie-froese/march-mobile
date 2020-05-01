2b8310a9fc66fb49a19317bbeaa85f86981cd6fd
dennie-froese <denfroese@gmail.com>
2020-05-01 09:50:45 +0100

    added default route to stack navigator

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
