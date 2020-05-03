71f0f5db9dd6b2238bb122f1b09196dc09943a8f
dennie-froese <denfroese@gmail.com>
2020-05-03 16:35:56 +0100

    WIP: adding toggle logic to furtherAccountDetails component

--- src/components/FurtherAccountDetails/index.tsx
+++ src/components/FurtherAccountDetails/index.tsx
@@ -1,15 +1,72 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+  Easing,
+} from 'react-native';
 
 export default function FurtherAccountDetails() {
+  const [state, setState] = useState<
+    null | 'growing' | 'doneGrowing' | 'shrinking' | 'doneShrinking'
+  >(null);
+  const height = useRef(new Animated.Value(0));
+
+  useEffect(() => {
+    console.warn(`asd: ${state}`);
+    if (state === 'growing') {
+      Animated.timing(height.current, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.linear),
+      }).start(() => {
+        setState('doneGrowing');
+      });
+    }
+    if (state === 'shrinking') {
+      Animated.timing(height.current, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.linear),
+      }).start(() => {
+        setState('doneShrinking');
+      });
+    }
+  }, [state]);
+
   return (
-    <View style={styles.furtherAccountDetails}>
-      <Text style={styles.furtherAccountDetailsText}>
-        Further account details
-      </Text>
-      <TouchableOpacity>
-        <Text>Show</Text>
-      </TouchableOpacity>
+    <View>
+      <View style={styles.furtherAccountDetails}>
+        <Text style={styles.furtherAccountDetailsText}>
+          Further account details
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            if (!state) {
+              setState('growing');
+            } else {
+              setState('shrinking');
+            }
+          }}>
+          <Text>Show</Text>
+        </TouchableOpacity>
+      </View>
+      {state === 'growing' ||
+      state === 'doneGrowing' ||
+      state === 'shrinking' ? (
+        <Animated.View
+          style={{
+            ...styles.animatedView,
+            ...{transform: [{scaleY: height.current}]},
+          }}>
+          <Text> Go</Text>
+        </Animated.View>
+      ) : null}
+      <Text> Test</Text>
     </View>
   );
 }
@@ -24,5 +81,9 @@ const styles = StyleSheet.create({
   furtherAccountDetailsText: {
     fontWeight: 'bold',
     fontSize: 15,
+  },
+  animatedView: {
+    backgroundColor: 'green',
+    width: '100%',
   },
 });
