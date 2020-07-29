bed45f78c70f3e26b0857bc7b09ca5299cb110aa
dennie-froese <denfroese@gmail.com>
2020-07-29 17:30:33 +0100

    added textInput fields to login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,70 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
 import PrimaryButton from 'components/PrimaryButton';
 import {useNavigation} from '@react-navigation/core';
 
 export default function Login() {
   const navigation = useNavigation();
+  const [user, setUser] = useState('');
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Login</Text>
-      <PrimaryButton
-        title="Log on!"
-        onPress={() => navigation.navigate('Balances')}
-      />
+      <View style={styles.topContainer}>
+        <Text style={styles.text}>Login</Text>
+        <View style={styles.inputsContainer}>
+          <Text style={styles.text}>User name</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setUser(text)}
+            value={user}
+            placeholder={'Enter your user name'}
+          />
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setUser(text)}
+            value={user}
+            placeholder={'Enter your password'}
+          />
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <PrimaryButton
+          title="Log on!"
+          onPress={() => navigation.navigate('Balances')}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'space-around',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  bottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  inputsContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    textAlign: 'center',
   },
   text: {
     textAlign: 'center',
