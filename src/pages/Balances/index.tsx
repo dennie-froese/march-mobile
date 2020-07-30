6aa0f6848985efd48b08ab6d2986f40b29921b59
dennie-froese <denfroese@gmail.com>
2020-07-30 17:06:56 +0100

    useFocusEffect hook to prevent back navigation from balances page

--- src/pages/Balances/index.tsx
+++ src/pages/Balances/index.tsx
@@ -1,9 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text, StatusBar} from 'react-native';
+import {StyleSheet, View, Text, StatusBar, BackHandler} from 'react-native';
 import AccountContainer from 'components/AccountContainer';
+import {useFocusEffect} from '@react-navigation/core';
 
 export default function Balances() {
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        return true;
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, []),
+  );
   return (
     <View>
       <Text style={styles.text}>Please find your account balances below:</Text>
