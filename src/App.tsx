7e1cfdb8d7378581b5b95d2b6e02299179c501fb
dennie-froese <denfroese@gmail.com>
2020-08-03 07:32:13 +0100

    minor design changes to header styles for stackNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {StackHeaderLeftButtonProps} from '@react-navigation/stack/lib/typescript/src/types';
 import Login from 'pages/Login';
 import Balances from 'pages/Balances';
 import Transactions from 'pages/Transactions';
-import {StackHeaderLeftButtonProps} from '@react-navigation/stack/lib/typescript/src/types';
 import {colours} from 'config/Theme';
 
 const Stack = createStackNavigator();
@@ -12,20 +12,18 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerStyle: {
-              backgroundColor: colours.orange,
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-        />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colours.primary,
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen name="Login" component={Login} options={{title: ''}} />
         <Stack.Screen
           name="Balances"
           component={Balances}
