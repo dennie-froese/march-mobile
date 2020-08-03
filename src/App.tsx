1888938fa4d1ccaf055cff226481613bbe912d24
dennie-froese <denfroese@gmail.com>
2020-08-03 20:56:46 +0100

    implementation of two stack navigators and integration into appStack nav in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import AuthenticationNavigator from 'navigation/Authentication';
 import {createStackNavigator} from '@react-navigation/stack';
-import {StackHeaderLeftButtonProps} from '@react-navigation/stack/lib/typescript/src/types';
-import Login from 'pages/Login';
-import Balances from 'pages/Balances';
-import Transactions from 'pages/Transactions';
-import {colours} from 'config/Theme';
+import BalancesNavigator from './navigation/Balances';
+import {NavigationContainer} from '@react-navigation/native';
 
-const Stack = createStackNavigator();
+const AppStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: colours.primary,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}>
-        <Stack.Screen name="Login" component={Login} options={{title: ''}} />
-        <Stack.Screen
-          name="Balances"
-          component={Balances}
-          options={{headerLeft: (props: StackHeaderLeftButtonProps) => null}}
+      <AppStack.Navigator headerMode="none">
+        <AppStack.Screen
+          name="Authentication"
+          component={AuthenticationNavigator}
         />
-        <Stack.Screen name="Transactions" component={Transactions} />
-      </Stack.Navigator>
+        <AppStack.Screen name="Balances" component={BalancesNavigator} />
+      </AppStack.Navigator>
     </NavigationContainer>
   );
 }
