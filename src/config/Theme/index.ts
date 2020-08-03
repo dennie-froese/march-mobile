851d95a083665987da9582a718b550f5f16b9dfa
dennie-froese <denfroese@gmail.com>
2020-08-03 07:34:31 +0100

    added theme to config providing colours, padding and borderRadius

---
+++ src/config/Theme/index.ts
@@ -0,0 +1,24 @@
+export const colours = {
+  primary: '#2CB9B0',
+  secondary: '#0C0D34',
+  danger: '#FF0058',
+  text: 'rgba(12, 13, 52, 0.7)',
+  white: 'white',
+  grey: '#F4F0EF',
+  orange: '#FE5E33',
+  yellow: '#FFC641',
+  pink: '#FF87A2',
+  violet: '#442CB9',
+};
+export const spacing = {
+  s: 8,
+  m: 16,
+  l: 24,
+  xl: 40,
+};
+export const borderRadii = {
+  s: 4,
+  m: 10,
+  l: 25,
+  xl: 75,
+};
