95d5b3fb558253055e603562bb8427f6a1421b20
dennie-froese <denfroese@gmail.com>
2020-08-03 07:49:21 +0100

    improved mockup of transaction component

--- src/components/Transaction/index.tsx
+++ src/components/Transaction/index.tsx
@@ -3,11 +3,22 @@ import {View, Text, StyleSheet} from 'react-native';
 
 export default function Transaction() {
   return (
-    <View style={styles.transactionContainer}>
-      <Text> Transaction 1</Text>
-      <Text> Value</Text>
-      <Text> Day</Text>
-    </View>
+    <>
+      <View style={styles.transactionContainer}>
+        <Text>Fri 31</Text>
+        <View style={styles.bottomContainer}>
+          <Text style={{fontWeight: 'bold'}}>Lisa Henderson</Text>
+          <Text>-£256.99</Text>
+        </View>
+      </View>
+      <View style={styles.transactionContainer}>
+        <Text>Tue 4</Text>
+        <View style={styles.bottomContainer}>
+          <Text style={{fontWeight: 'bold'}}>George Hewitt</Text>
+          <Text>-£56.50</Text>
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -15,5 +26,10 @@ const styles = StyleSheet.create({
   transactionContainer: {
     paddingTop: 10,
     paddingBottom: 10,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 5,
   },
 });
