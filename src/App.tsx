eaa95fc73e0611ef877d5d4d019262843e31975a
dennie-froese <denfroese@gmail.com>
2020-08-04 18:16:40 +0100

    implemented new Home navigator in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AuthenticationNavigator from 'navigation/Authentication';
+import HomeNavigator from 'navigation/Home';
 import {createStackNavigator} from '@react-navigation/stack';
-import BalancesNavigator from './navigation/Balances';
 import {NavigationContainer} from '@react-navigation/native';
 
 const AppStack = createStackNavigator();
@@ -14,7 +14,7 @@ export default function App() {
           name="Authentication"
           component={AuthenticationNavigator}
         />
-        <AppStack.Screen name="Balances" component={BalancesNavigator} />
+        <AppStack.Screen name="Home" component={HomeNavigator} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
