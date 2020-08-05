8f48020b4e6d74fede1b7aed3987ed4b3bf78b1f
dennie-froese <denfroese@gmail.com>
2020-08-05 19:35:36 +0100

    implemented balances and settings icon in tab navigator

--- src/navigation/Home/index.tsx
+++ src/navigation/Home/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import BalancesNavigator from 'navigation/Balances';
 import Settings from 'pages/Settings';
-import Lock from 'icons/Lock';
-import Mail from 'icons/Mail';
+import BalancesIcon from 'icons/Balances';
+import SettingsIcon from 'icons/Settings';
 import {colours} from 'config/Theme';
 import {View} from 'react-native';
 
@@ -15,27 +15,27 @@ export default function HomeNavigator() {
       tabBarOptions={{
         activeTintColor: colours.primary,
         inactiveTintColor: colours.secondary,
-        tabStyle: {paddingTop: 5},
+        tabStyle: {paddingTop: 10},
         allowFontScaling: true,
         labelStyle: {fontSize: 16, padding: 5},
         style: {
           height: 60,
           borderTopColor: colours.primary,
-          borderTopWidth: 4,
+          borderTopWidth: 2,
         },
       }}>
       <Home.Screen
         name="Balances"
         component={BalancesNavigator}
         options={{
-          tabBarIcon: props => <Lock width={20} height={20} />,
+          tabBarIcon: props => <BalancesIcon width={20} height={20} />,
         }}
       />
       <Home.Screen
         name="Settings"
         component={Settings}
         options={{
-          tabBarIcon: props => <Lock width={20} height={20} />,
+          tabBarIcon: props => <SettingsIcon width={20} height={20} />,
         }}
       />
     </Home.Navigator>
