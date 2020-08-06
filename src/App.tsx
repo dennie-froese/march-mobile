feb678d11e0ff67a711af2a4ceb80ebef9ada552
dennie-froese <denfroese@gmail.com>
2020-08-06 18:54:58 +0100

    wrapped app in context stateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import AuthenticationNavigator from 'navigation/Authentication';
 import HomeNavigator from 'navigation/Home';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import {StateProvider} from './hooks/useState';
 
 const AppStack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen
-          name="Authentication"
-          component={AuthenticationNavigator}
-        />
-        <AppStack.Screen name="Home" component={HomeNavigator} />
-      </AppStack.Navigator>
-    </NavigationContainer>
+    <StateProvider>
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen
+            name="Authentication"
+            component={AuthenticationNavigator}
+          />
+          <AppStack.Screen name="Home" component={HomeNavigator} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    </StateProvider>
   );
 }
